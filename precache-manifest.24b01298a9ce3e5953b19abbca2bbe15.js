4f7785b1cd9e2f2d91088d0a2a3e1617950a0c9e
6012hsaka <akashsharma15105@gmail.com>
2020-08-25 22:11:58 +0530

    Updates

---
+++ precache-manifest.24b01298a9ce3e5953b19abbca2bbe15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a90ac649996160534ba789b362307d4",
+    "url": "/expenditure-tracker/index.html"
+  },
+  {
+    "revision": "bad5610b7f552b066e9f",
+    "url": "/expenditure-tracker/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "62485782745bfe7b142c",
+    "url": "/expenditure-tracker/static/css/main.c8e98290.chunk.css"
+  },
+  {
+    "revision": "bad5610b7f552b066e9f",
+    "url": "/expenditure-tracker/static/js/2.9d06d8d5.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/expenditure-tracker/static/js/2.9d06d8d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62485782745bfe7b142c",
+    "url": "/expenditure-tracker/static/js/main.0455b6bd.chunk.js"
+  },
+  {
+    "revision": "4f68af160a4b59a09db4",
+    "url": "/expenditure-tracker/static/js/runtime-main.7446e4a9.js"
+  }
+]);
